ba4a96173d8b41c0a6baa2c068706f8c70ddb618
8385216 <rebekah.dawson@g.austinn.edu>
2019-01-24 19:17:12 -0600

    Homework 1 - write multiply numbers function

--- 01-basics/basics.h
+++ 01-basics/basics.h
@@ -1,5 +1,6 @@
 ﻿int sum_numbers(int num1, int num2);
 
 //write function prototype for multiply_numbers with two integer parameters
+int multiply_numbers(int num1, int num2);
 
 
--- 01-basics_test/basics_test.cpp
+++ 01-basics_test/basics_test.cpp
@@ -8,8 +8,8 @@ TEST_CASE("Test sum_numbers", "[1,5,10]") {
 	REQUIRE(sum_numbers(5, 5) == 10);
 }
 
-//Using the Test case above as a guide, write a test case for the
-//multiply_numbers with values 
-//2, 2 to equal 4
-//5, 5 to equal 25
-//10,10 to equal 100
+TEST_CASE("Test multiply_numbers","[2,5,10]") {
+	REQUIRE(multiply_numbers(2, 2) == 4);
+	REQUIRE(multiply_numbers(5, 5) == 25);
+	REQUIRE(multiply_numbers(10, 10) == 100);
+}
