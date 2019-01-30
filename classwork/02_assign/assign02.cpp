434a38ac0e1e77d5bfebfb1edf7f0f8b33b40f30
Rebekah <rebekah.dawson@g.austincc.edu>
2019-01-29 19:07:25 -0600

    classwork assignment 1/29

--- classwork/02_assign/assign02.cpp
+++ classwork/02_assign/assign02.cpp
@@ -1,1 +1,5 @@
 //Write the function code that returns the product of hours and hourly_rate.
+double gross_pay(double hours, double hourly_rate)
+{
+	return hours * hourly_rate;
+}
--- classwork/classwork_tests/class_tests.cpp
+++ classwork/classwork_tests/class_tests.cpp
@@ -7,13 +7,10 @@ TEST_CASE("Verify Test Configuration", "verification") {
 }
 
 
-//Write the test case for the function gross_pay with values:
-//hours 10 hourly_rate 15 result should be 150
-//hours  5 hourly_rate 20 result should be 100
-//hours 20 hourly_rate 10 result should be 200 
-
-TEST_CASE("name assign 1 function", "[150,100,200]") {
-	//sample to guide you ---> REQUIRE(gross_pay(5, 5) == 25);
+TEST_CASE("assign02_test", "[150,100,200]") {
+	REQUIRE(gross_pay(10, 15) == 150);
+	REQUIRE(gross_pay(5, 20) == 100);
+	REQUIRE(gross_pay(20, 10) == 200);
 	
 }
 
