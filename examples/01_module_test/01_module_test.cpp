af0801d22717bd059f02a318eacde199208635dc
Rebekah <rebekah.dawson@g.austincc.edu>
2019-01-31 19:26:06 -0600

    in-class assignment 1/31

--- examples/01_module_test/01_module_test.cpp
+++ examples/01_module_test/01_module_test.cpp
@@ -1,23 +1,35 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "output.h"
+#include "if.h"
+#include "if_else.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-//Create a test case for the if statement using:
-//the number 4 result should be true
-//the number 55 result should be false
+TEST_CASE("Test is even")
+{
+	REQUIRE(is_even(4) == true);
+	REQUIRE(is_even(55) == false);
+}
 
-//Create a test case for the if else statement using:
-//the year 1930 result should be 'Silent Generation'
-//the year 1946 result should be 'Baby boomer'
-//the year 1970 result should be 'Generation X'
-//the year 1995 result should be 'Millenial'
-//the year 1996 result should be 'Centennial'
-//the year 1900 result should be 'Invalid Year'
-//the year 2019 result should be 'Invalid Year'
+TEST_CASE("Test Overtime Function")
+{
+	REQUIRE(overtime(41) == true);
+	REQUIRE(overtime(24) == false);
+}
+
+TEST_CASE("Test Generation Function")
+{
+	REQUIRE(get_generation(1930) == "Silent Generation");
+	REQUIRE(get_generation(1946) == "Baby Boomer");
+	REQUIRE(get_generation(1970) == "Generation X");
+	REQUIRE(get_generation(1995) == "Millenial");
+	REQUIRE(get_generation(1996) == "Centennial");
+	REQUIRE(get_generation(1900) == "Invalid Year");
+	REQUIRE(get_generation(2019) == "Invalid Year");
+}
 
 
 //Create a test case for the switch statement using:
