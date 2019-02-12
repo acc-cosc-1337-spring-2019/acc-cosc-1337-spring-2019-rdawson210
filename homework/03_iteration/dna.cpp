116dbe4772f6194c69f02175921d77268d1f8640
rdawson210 <rebekah.dawson@g.austincc.edu>
2019-02-11 18:20:48 -0600

    HW 3 in progress

--- homework/03_iteration/dna.cpp
+++ homework/03_iteration/dna.cpp
@@ -1,3 +1,5 @@
+#include<iostream>
+using std::string;
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -5,13 +7,31 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
-
+double get_gc_content(string input)
+{
+	return 0.0;
+}
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
-
+string get_reverse_string(string input)
+{
+	string str = input;
+	double len = str.length();
+	int j = len - 1;
+	int i = 0;
+	char temp;
+	do {
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		++i;
+		--j;
+	} while (i < j);
+	return str;
+}
 
 /*
 Write prototype for function get_dna_complement that
@@ -19,7 +39,23 @@ accepts a string dna and returns a string.
 Calculate dna complement:
 a. call function get_reverse_string(dna), save results to a local string variable
 b. iterate local string variable and
-    replace A with T, T with A, C with G and G with C
+	replace A with T, T with A, C with G and G with C
 c. return string
 
 */
+string get_dna_complement(string input)
+{
+	string str = input;
+	str = get_reverse_string(str);
+	int len = str.length();
+	for (int i = 0; i <= len; i++)
+		if (str[i] == 'A')
+			str[i] = 'T';
+		else if (str[i] == 'T')
+			str[i] = 'A';
+		else if (str[i] == 'C')
+			str[i] = 'G';
+		else if (str[i] == 'G')
+			str[i] = 'C';
+	return str;
+}
--- homework/03_iteration_test/iteration_test.cpp
+++ homework/03_iteration_test/iteration_test.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include"dna.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -22,6 +23,10 @@ Value	Result
 AGCTATAG	GATATCGA
 CGCTATAG	GATATCGC
 */
+TEST_CASE("Testing get_reverse_string") {
+	REQUIRE(get_reverse_string("AGCTATAG") == "GATATCGA");
+	REQUIRE(get_reverse_string("CGCTATAG") == "GATATCGC");
+}
 
 
 
@@ -32,3 +37,7 @@ Value	Result
 AAAACCCGGT	ACCGGGTTTT
 CCCGGAAAAT	ATTTTCCGGG
 */
+TEST_CASE("Testing get_dna_complement") {
+	REQUIRE(get_dna_complement("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT") == "ATTTTCCGGG");
+}
