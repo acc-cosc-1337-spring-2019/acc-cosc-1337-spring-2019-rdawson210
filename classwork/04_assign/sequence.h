991faa40e4d27e5c4c42e1c1521d98c6784be0c2
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-02-14 15:09:48 -0600

    Assignment 4 starter files

---
+++ classwork/04_assign/sequence.h
@@ -0,0 +1,10 @@
+/*
+Write prototype for void function display_vector that accepts a parameter const reference vector of strings
+*/
+
+
+/*
+Write prototype for void function update_vector_element that accepts parameters reference vector of strings,
+a string vector search_value, and a string replace_value.
+*/
+
