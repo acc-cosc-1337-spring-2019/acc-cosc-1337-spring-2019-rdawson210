21887c7da77d6d99b93d29988526b214433cdac0
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-03-01 21:59:35 -0600

    Homework 6 starter files

---
+++ homework/tic_tac_toe/tic_tac_toe_manager.cpp
@@ -0,0 +1,2 @@
+#include "tic_tac_toe_manager.h"
+//Write class function implementations here
---
+++ homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -0,0 +1,6 @@
+#ifndef TIC_TAC_TOE_MANAGER_H
+#define TIC_TAC_TOE_MANAGER_H
+//Write class interface here
+
+
+#endif // !TIC_TAC_TOE_MANAGER_H
