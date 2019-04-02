a72ecaa4d491dc993309d169118efef31a5f2115
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-04-02 17:16:45 -0500

    wxWidgets module 8 starter files

---
+++ examples/08_module/panel.cpp
@@ -0,0 +1,14 @@
+#include "panel.h"
+
+/*
+Class Constructor
+
+@param wxWindow* the parent window for the Panel class
+*/
+Panel::Panel(wxWindow* parent) 
+	: wxPanel(parent, -1)
+{
+	
+}
+
+
---
+++ examples/08_module/panel.h
@@ -0,0 +1,17 @@
+#ifndef PANEL_H
+#define PANEL_H
+#include<wx/wx.h>
+#include<vector>
+#include<string>
+#include<memory>
+
+class Panel : public wxPanel
+{
+public:
+	Panel(wxWindow* parent);
+	
+private:
+
+};
+
+#endif // !PANEL_H
