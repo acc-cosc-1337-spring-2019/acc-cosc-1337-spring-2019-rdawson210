162c62cd3fee73b80f4561126a795274a3638df6
Rebekah <rebekah.dawson@g.austincc.edu>
2019-04-04 18:36:06 -0500

    classwork 4/4

--- examples/04_module/gui/panel.cpp
+++ examples/04_module/gui/panel.cpp
@@ -8,4 +8,55 @@ Class Constructor
 Panel::Panel(wxWindow* parent) 
 	: wxPanel(parent, -1)
 {
+	account = std::make_unique<CheckingAccount>(123, 500);
+
+	auto vbox = get_box_sizer();
+	SetSizer(vbox);
+}
+
+void Panel::on_submit_button_click(wxCommandEvent & event)
+{
+	if (trans_type_radio->GetSelection() == 0)
+	{
+		account->deposit(std::stod(transaction_text->GetValue().ToStdString()));
+	}
+	else if (trans_type_radio->GetSelection() == 1)
+	{
+		account->withdraw(std::stod(transaction_text->GetValue().ToStdString()));
+	}
+
+	balance_text->SetLabelText(wxString(std::to_string(account->get_balance())));
+}
+/*
+Box Sizer - main bank account interface
+*/
+wxBoxSizer * Panel::get_box_sizer()
+{
+	auto vbox1 = new wxBoxSizer(wxVERTICAL);
+	auto balance_label = new wxStaticText(this, wxID_ANY, wxString("Balance: "),
+		wxDefaultPosition, wxSize(70, -1));
+	vbox1->Add(balance_label, 0);
+
+	balance_text = new wxStaticText(this, wxID_ANY, wxString(std::to_string(account->get_balance())),
+		wxDefaultPosition, wxSize(70, -1));
+	vbox1->Add(balance_text, 0);
+
+	wxString trans_type_choices[]{ wxT("Deposit"), wxT("Withdraw") };
+
+	trans_type_radio = new wxRadioBox(this, -1, wxT("Trans Type"), wxDefaultPosition, wxDefaultSize,
+		WXSIZEOF(trans_type_choices), trans_type_choices, 1, wxRA_SPECIFY_COLS);
+	vbox1->Add(trans_type_radio, 0);
+
+	auto amount_label = new wxStaticText(this, wxID_ANY, wxT("Amount: "), wxDefaultPosition, 
+										wxSize(70, -1));
+	vbox1->Add(amount_label, 0);
+
+	transaction_text = new wxTextCtrl(this, wxID_ANY);
+	vbox1->Add(transaction_text, 0);
+
+	submit_button = new wxButton(this, -1, wxT("Submit"));
+	submit_button->Bind(wxEVT_BUTTON, &Panel::on_submit_button_click, this);
+	vbox1->Add(submit_button);
+
+	return vbox1;
 }
--- examples/04_module/gui/panel.h
+++ examples/04_module/gui/panel.h
@@ -1,6 +1,7 @@
 #ifndef PANEL_H
 #define PANEL_H
 #include<wx/wx.h>
+#include"checking_account.h"
 
 
 class Panel : public wxPanel
@@ -9,6 +10,14 @@ public:
 	Panel(wxWindow* parent);
 	
 private:
+	void on_submit_button_click(wxCommandEvent & event);
+	wxBoxSizer* get_box_sizer();
+	wxStaticText* balance_label;
+	wxTextCtrl* transaction_text;
+	wxButton* submit_button;
+	wxStaticText* balance_text;
+	wxRadioBox* trans_type_radio;
+	std::unique_ptr<BankAccount> account;
 
 };
 
