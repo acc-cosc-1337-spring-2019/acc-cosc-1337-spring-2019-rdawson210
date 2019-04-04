0b96d3aa12a38352ad6ce012dfc40123b58e621a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-04-04 12:31:37 -0500

    Bank GUI Example

---
+++ examples/04_module/gui/panel.cpp
@@ -0,0 +1,11 @@
+#include "panel.h"
+#include<string>
+/*
+Class Constructor
+
+@param wxWindow* the parent window for the Panel class
+*/
+Panel::Panel(wxWindow* parent) 
+	: wxPanel(parent, -1)
+{
+}
---
+++ examples/04_module/gui/panel.h
@@ -0,0 +1,15 @@
+#ifndef PANEL_H
+#define PANEL_H
+#include<wx/wx.h>
+
+
+class Panel : public wxPanel
+{
+public:
+	Panel(wxWindow* parent);
+	
+private:
+
+};
+
+#endif // !PANEL_H
